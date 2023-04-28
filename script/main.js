a9b5a0cbb8f85ce27a9636621122be8a4815074a
LouisCantinieaux <cantinieauxlouis@gmail.com>
2023-04-28 10:03:12 +0200

    Ajout du code HTML/CSS, début JS

--- script/main.js
+++ script/main.js
@@ -1,1 +1,24 @@
+let menu = [];
+menu[0]="<ul><li>1.1</li><li>1.2</li></ul>";
+menu[1]="<ul><li>2.1</li><li>2.2</li></ul>";
+menu[2]="<ul><li>3.1</li><li>3.2</li><li>3.3</li><li>3.4</li></ul>";
+menu[3]="<ul><li>4.1</li><li>4.2</li></ul>";
 
+let liste;
+
+let index = -1;
+let ligne ="";
+
+function init(){
+    liste = document.getElementById("carte").getElementsByTagName("li");
+
+}
+
+function debut(i){
+    if(index >= 0){
+        liste[i].innerHTML = ligne;
+    }
+    ligne = liste[i].innerHTML;
+    index = i;
+    liste[i].innerHTML += menu[i];
+}
