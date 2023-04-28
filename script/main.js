6bd39e381ff38597f49ebf93891de6e8ab403adf
LouisCantinieaux <cantinieauxlouis@gmail.com>
2023-04-28 12:10:37 +0200

    modifi de la carte, css, js

--- script/main.js
+++ script/main.js
@@ -4,8 +4,9 @@ menu[1]="<ul><li>2.1</li><li>2.2</li></ul>";
 menu[2]="<ul><li>3.1</li><li>3.2</li><li>3.3</li><li>3.4</li></ul>";
 menu[3]="<ul><li>4.1</li><li>4.2</li></ul>";
 
+const validation = document.getElementById("validation");
 let liste;
-
+let tabRempli = [];
 let index = 0;
 let ligne ="";
 
@@ -21,3 +22,15 @@ function debut(i){
     index = i;
     liste[i].innerHTML += menu[i];
 }
+
+validation.addEventListener("click", function(){
+    //Envoi dans le local storage
+    localStorage.setItem("maCommande", JSON.stringify(menu));
+
+    //Récupération depuis local storage
+    tabRempli = JSON.parse(localStorage.getItem("maCommande"));
+
+    for(let i = 0; i < tabRempli.length; i++){
+        console.log("élément" + i + " " + tabRempli[i]);
+    }
+})
